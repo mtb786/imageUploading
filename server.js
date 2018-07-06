8a6db344f73f790853a36bc8bc723f0eb0ad6289
Manish Bhavnani <bhavnani099@gmail.com>
2018-07-05 21:33:54 -0700

    Added a Procfile.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.use('*',(req,res) => {
-console.log('hello');
+console.log('Hello Karan ');
 res.send('hello');
 } );
 const PORT = process.env.PORT ||'8080';
